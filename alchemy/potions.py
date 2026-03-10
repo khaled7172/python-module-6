eeb8d22cecc72ee947b823d49ffbe1ff05d5ce08
khaled <khaledhmmddd123@gmail.com>
2026-03-10 15:23:42 +0200

    potion boy

---
+++ alchemy/potions.py
@@ -0,0 +1,16 @@
+from . import elements
+
+def healing_potion() -> str:
+    return f"Healing potion brewed with {elements.create_fire()} and {elements.create_water()}"
+
+
+def strength_potion() -> str:
+    return f"Strength potion brewed with {elements.create_earth()} and {elements.create_fire()}"
+
+
+def invisibility_potion() -> str:
+    return f"Invisibility potion brewed with {elements.create_air()} and {elements.create_water()}"
+
+
+def wisdom_potion() -> str:
+    return f"Wisdom potion brewed with all elements: {elements.create_water()} and {elements.create_air()} and {elements.create_earth()} and {elements.create_fire()}"
